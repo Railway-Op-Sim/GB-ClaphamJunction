98a702ec17b21719467cbafbb9e5999cb615bb43
Daniel-Gill <56364196+Daniel-Gill@users.noreply.github.com>
2020-12-20 20:55:13 +0000

    Timetable infrastructure

---
+++ Documentation/TTBuilder/MakeTTWAK.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+java -jar "C:\Users\dan23\Desktop\TTbuilder-master\TTbuilder-master\dist\TTBuilder.jar" -o=ClaphamJunction.ttb -i=CLJ_Head.xml -i=CLJ_Main.xml
+
+
+read -p "Pause: " name
